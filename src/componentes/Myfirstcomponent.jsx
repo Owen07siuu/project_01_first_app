d092d9178fbc37bfd018f85c0793d762690f380d
Owen07 <owqn2005@gmail.com>
2024-07-05 07:58:40 -0500

    my first  component

---
+++ src/componentes/Myfirstcomponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Myfirstcomponent = () => {
+  return (
+    <div>
+    <div>myfirstcomponent</div>
+    <div>Helllo world, my name is owen Smith</div>
+    <div>5  de julio del 2024</div>
+    </div>
+  )
+}
